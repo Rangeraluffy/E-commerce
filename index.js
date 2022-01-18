a8dcd75008120a255f928dce6cbe34bcd76e07d5
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-18 16:51:26 +0100

    Set env mongo Url

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-mongoose.connect(
-    "mongodb+srv://user:<password>@cluster0.qa1dq.mongodb.net/shop?retryWrites=true&w=majority"
-    )
-.then(() => console.log("DBConnection Successfull!"))
-.catch((err)=>{
-    console.log(err);
-});
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DBConnection Successfull!"))
+    .catch((err)=>{
+        console.log(err);
+    });
 
 app.listen(5000, ()=>{
     console.log("Backend end server is running!");
