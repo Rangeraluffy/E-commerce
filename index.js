ec3faeb20248a340479c27c193cca19c4e37d7bf
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-18 17:06:44 +0100

    Testing route for user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
 
 dotenv.config();
 
@@ -12,9 +13,7 @@ mongoose
         console.log(err);
     });
 
- app.get("/api/test", () => {
-    console.log("Test is successfull");
-});
+app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend end server is running!");
