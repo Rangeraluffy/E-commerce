1b41e2ea27c71cbe64b1176e1ffd90c289cb8b02
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-19 15:32:31 +0100

    implement JWT Update Readme

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ To get the Node server running locally:
 - [mongoose](https://github.com/Automattic/mongoose) - For modeling and mapping MongoDB data to javascript.
 - [nodeJs](https://github.com/nodejs/node) - Node.js is an open-source, cross-platform, JavaScript runtime environment.
 - [cryptoJs](https://github.com/brix/crypto-js) - JavaScript implementations of standard and secure cryptographic algorithms for my password.
+- [jsonWebToken](https://github.com/auth0/node-jsonwebtoken) - JSON Web Token (JWT) is a compact, URL-safe means of representing claims to be transferred between two parties.
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require ("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req,res) => {
@@ -39,11 +40,20 @@ router.post("/login", async (req, res) => {
     const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
     OriginalPassword != req.body.password && 
-            res.status(401).json("Wrong Password");
+    res.status(401).json("Wrong Password");
+
+    const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
 
     const { password, ...others } = user._doc; 
 
-    res.status(200).json(others);
+    res.status(200).json({others, accessToken});
     } catch (err) {
         res.status(500).json(err);
     }
