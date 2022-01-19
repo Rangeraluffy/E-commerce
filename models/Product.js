ecb211470d8eca95e0ba88b340a98d949d769fe0
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-19 09:31:09 +0100

    Create Model Cart

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
-const { boolean } = require ("webidl-conversions");
 
 const ProductSchema = new mongoose.Schema(
     {
-        title:{type:String, required:true, unique:true},
-        description:{type:String, required:true},
-        img:{type:String, required:true},
-        categories:{type: Array},
-        size:{type:String },
-        color:{type:String },
-        price:{type:Number, required:true},
+        title:{ type : String, required : true, unique : true},
+        desc:{ type : String, required : true},
+        img:{ type : String, required : true},
+        categories:{ type :  Array},
+          size:{ type : String },
+          color:{ type : String },
+          price:{ type : Number, required : true},
     },
     {timestamps: true}
 );
