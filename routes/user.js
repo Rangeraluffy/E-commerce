2408697983d76e1956197a89ad74b6d750a26a15
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-19 09:17:31 +0100

    Routes for user name with postman

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,10 @@ router.get("/usertest", (req,res) => {
     res.send("user test is successfull");
 });
 
+router.post("/userposttest", (req,res) => {
+    const username = req.body.username
+    res.send("Your username is : " + username)
+})
+
 
 module.exports = router
