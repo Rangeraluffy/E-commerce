56460d1c946621f0ce36a39b7faf56ef9d2774c5
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-20 10:22:19 +0100

    Token and Users Postman

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
